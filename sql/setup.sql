bf486bf0056219caf46ce9b88d1434fc84547a67
Sean Moriarty <spmoriarty1@gmail.com>
2022-07-27 12:15:40 -0700

    sql setup hydrated

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
+DROP table if exists cats;
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+CREATE table cats (
+    id BIGINT GENERATED ALWAYS AS IDENTIY,
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL,
+    year INT,
+    lives INT,
+    isSidekick VARCHAR NOT NULL
+);
+
+INSERT INTO cats (name, type, year, lives, isSidekick) VALUES
+('Felix', 'Tuxedo', 1892, 3, 'false'),
+('Garfield', 'Orange Tabby', 1978, 7, 'false'),
+('Duchess', 'Angora', 1970, 9, 'false'),
+('Stimpy', 'Manx', 1990, 1, 'true');
